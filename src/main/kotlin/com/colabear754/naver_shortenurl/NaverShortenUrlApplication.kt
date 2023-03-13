17ad10e643456d2d1485074acb7e8ac3e30132a4
Colabear754 <jky2406@gmail.com>
2023-03-13 13:24:20 +0900

    Initial commit

---
+++ src/main/kotlin/com/colabear754/naver_shortenurl/NaverShortenUrlApplication.kt
@@ -0,0 +1,11 @@
+package com.colabear754.naver_shortenurl
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NaverShortenUrlApplication
+
+fun main(args: Array<String>) {
+    runApplication<NaverShortenUrlApplication>(*args)
+}
---
+++ src/test/kotlin/com/colabear754/naver_shortenurl/NaverShortenUrlApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.colabear754.naver_shortenurl
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NaverShortenUrlApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
