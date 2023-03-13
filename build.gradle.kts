5097aeeb3030fa41e67d633602ab8d6284a64622
Colabear754 <jky2406@gmail.com>
2023-03-13 14:01:36 +0900

    bootJar 빌드 파일명 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,13 +30,13 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "18"
+tasks {
+    withType<KotlinCompile> {
+        kotlinOptions {
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+            jvmTarget = "18"
+        }
     }
-}
-
-tasks.withType<Test> {
-    useJUnitPlatform()
+    withType<Test> { useJUnitPlatform() }
+    bootJar { archiveFileName.set("naver_shortenUrl.jar") }
 }
