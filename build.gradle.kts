8d39e784353a1e6724f6adbfd9d2ad0c0da1e986
Colabear754 <jky2406@gmail.com>
2023-03-14 09:07:49 +0900

    자바버전 18->19

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 
 group = "com.colabear754"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_18
+java.sourceCompatibility = JavaVersion.VERSION_19
 
 repositories {
     mavenCentral()
